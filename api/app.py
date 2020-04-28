824b5093dce1f82e335c28e0d5f3cf06c27d81e7
Michael Diggin <michael.diggin@gmail.com>
2020-04-28 17:00:42 +0100

    web page start

--- api/app.py
+++ api/app.py
@@ -1,15 +1,17 @@
 import os
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import tempfile
 
 from api.rowing_model import RowingModel
 
 
-UPLOAD_FOLDER = tempfile.mkdtemp()
+UPLOAD_FOLDER = '..\web\images'
 ALLOWED_EXTENSIONS = ['jpg', 'png', 'jpeg']
 
 application = Flask(__name__)
+CORS(application)
 application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 application.config['MAX_CONTENT_SIZE'] = 16*1024*1024
 
@@ -22,15 +24,13 @@ def allowed_file(filename):
 @application.route('/v1/predict', methods=['GET', 'POST'])
 def file_upload():
     if request.files:
-        print(request.files.keys())
-        image = request.files['image']
-        print(image.filename)
+        image =request.files['image']
         if allowed_file(image.filename):
             filename = secure_filename(image.filename)
             img_path = os.path.join(application.config["UPLOAD_FOLDER"], image.filename)
             image.save(img_path)
             pred_class, prob = model.predict(img_path)
-            resp = jsonify({'class': pred_class, 'probability': f"{round(prob, 2)}%"})
+            resp = jsonify({'class': pred_class, 'probability': f"{round(prob, 2)}%", 'path': img_path})
         else:
             resp = make_response(jsonify(
                 {'error': 'Attached media not an image of the form jpg, png or jpeg'}), 400)
--- web/upload.js
+++ web/upload.js
@@ -1,18 +1,30 @@
 const api_url = 'http://127.0.0.1:5000/v1/predict'
 
 $(function() {
-  $('#upload-file-btn').click(function() {
-      var form_data = new FormData($('#upload-file')[0]);
+  $('#upload-image-btn').click(function() {
+      
+      var form_data = new FormData($('#upload-image')[0]);
       $.ajax({
           type: 'POST',
           url: api_url,
+          dataType: 'json',
           data: form_data,
           contentType: false,
           cache: false,
           processData: false,
-          success: function(data) {
-              console.log('Success!');
+          success: function(response) {
+              $('#msg').html('');
+              $.each(response, function (key, data) {
+                  $('#msg').append(`${key} : ${data}<br/>`);
+              })
+              img_path = response.path;
+              console.log(img_path);
+              $('#img-name').attr('src', img_path);
+              
           },
+          error: function(response) {
+              $('#msg').html(response.message);
+          }
       });
   });
 });
